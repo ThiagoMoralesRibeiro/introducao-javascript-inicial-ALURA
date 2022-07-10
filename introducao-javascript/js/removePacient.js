39fcef75882010cc121d8815196bef94c3720e22
ThiagoMRibeiro05 <thiagomoralesribeiro@gmail.com>
2022-07-09 21:55:42 -0300

    testeTbody

--- introducao-javascript/js/removePacient.js
+++ introducao-javascript/js/removePacient.js
@@ -1,6 +1,10 @@
 pacientes = document.querySelectorAll(".paciente");
 console.log(pacientes);
 
+table = document.getElementById("tabela-pacientes");
+console.log(table);
+table.addEventListener("click", function () { console.log("Clicked") })
+
 //exemplo com forEach
 /*pacientes.forEach( function(paciente)  {
     paciente.addEventListener("dblclick", function (){ 
@@ -10,11 +14,11 @@ console.log(pacientes);
 });*/
 
 
-for (let i = 0; i < pacientes.length; i++) {
+/*for (let i = 0; i < pacientes.length; i++) {
     const pacienteTr = pacientes[i];
     pacienteTr.addEventListener("dblclick", function () { 
         this.remove(); //this está atrelado ao dono do evento
     });
     
-}
+}*/
 
