e834d592fa76090cd14e93f5ec5e60cbafe20ee9
ThiagoMRibeiro05 <thiagomoralesribeiro@gmail.com>
2022-07-10 00:22:53 -0300

    effectFadeOut

--- introducao-javascript/js/removePacient.js
+++ introducao-javascript/js/removePacient.js
@@ -2,8 +2,18 @@ pacientes = document.querySelectorAll(".paciente");
 console.log(pacientes);
 
 table = document.getElementById("tabela-pacientes");
-console.log(table);
-table.addEventListener("click", function () { console.log("Clicked") })
+//console.log(table);
+table.addEventListener("dblclick", function (event) { 
+    targetPacient = event.target;
+    parentTarget = targetPacient.parentNode;
+    parentTarget.classList.add("fadeOut")
+    setTimeout(function () { 
+        parentTarget.remove(); //o timeout me permite visualizar o efeito de fadeout e depois apagar o elemento
+    }, 500)
+    
+
+    
+})
 
 //exemplo com forEach
 /*pacientes.forEach( function(paciente)  {
@@ -13,7 +23,7 @@ table.addEventListener("click", function () { console.log("Clicked") })
     
 });*/
 
-
+//esses exemplos não funcionam, visto que o evento anteriormente não estava no meu elemento pai
 /*for (let i = 0; i < pacientes.length; i++) {
     const pacienteTr = pacientes[i];
     pacienteTr.addEventListener("dblclick", function () { 
